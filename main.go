a5c7c51d213260821e8c35ec0017ea704de15f32
YoheiNishimura <york.nishi@gmail.com>
2021-07-03 17:14:23 +0900

    parse

--- main.go
+++ main.go
@@ -2,44 +2,108 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"regexp"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
+type DocTitleBody struct {
+	title string
+	body  string
+}
+
 // input: 和訳したい記事のURL, output: 和訳したい記事のhtmlファイル
-func httpRequest(url *string) []byte {
+func getDocument(url *string) DocTitleBody {
 	resp, err := http.Get(*url)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 		os.Exit(1)
 	}
 
-	b, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
-	//fmt.Printf("%s", b) // type of b is byte
-	return b
+
+	var od DocTitleBody
+	od.title = doc.Find("title").Text()
+	od.body = doc.Find("div > div.col-lg-9.col-md-9.col-mod-single.col-mod-main > div.row > div.col-lg-10.col-md-10.col-sm-10").Text()
+
+	resp.Body.Close()
+	return od
 
 }
 
-// input: 和訳したい記事のhtml, output:<title>, <body>を含んだStrings
-//func parse()
+//input: 和訳したい記事のtitle, body, output:<title>の中身, <body>（メイン文書の中身）をextractしたStrings
+func parse(od DocTitleBody) DocTitleBody {
+
+	r := regexp.MustCompile(`Subscribe to the Crunchbase Daily`)
+	od.body = r.ReplaceAllString(od.body, " ")
+	r = regexp.MustCompile(`Shares`)
+	od.body = r.ReplaceAllString(od.body, " ")
+	r = regexp.MustCompile(`Email`)
+	od.body = r.ReplaceAllString(od.body, " ")
+	r = regexp.MustCompile(`Facebook`)
+	od.body = r.ReplaceAllString(od.body, " ")
+	r = regexp.MustCompile(`Twitter`)
+	od.body = r.ReplaceAllString(od.body, " ")
+	r = regexp.MustCompile(`LinkedIn`)
+	od.body = r.ReplaceAllString(od.body, " ")
+	r = regexp.MustCompile(`\s{2}`)
+	od.body = r.ReplaceAllString(od.body, " ")
+
+	r = regexp.MustCompile(`– Crunchbase News`)
+	od.title = r.ReplaceAllString(od.title, "")
+
+	return od
+
+}
 
 // input: Strings, output: 和訳が含まれるJSON
-//func deeplRequest()
+// 以下は適当に貼り付けしただけだから、Deeplのhttp reqの形に合わせて変更する
+func HttpPost(url, token, device string) error {
+	url =
+	values := url.Values{}
+	values.Set("token", token)
+	values.Add("device", device)
+
+	req, err := http.NewRequest(
+			"POST",
+			url,
+			strings.NewReader(values.Encode()),
+	)
+	if err != nil {
+			return err
+	}
+
+	// Content-Type 設定
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+			return err
+	}
+	defer resp.Body.Close()
+
+	return err
+}
 
 // input: 和訳が含まれるJSON、output: htmlファイル
 //func produceHtml()
 
 func main() {
-	//var url string = "https://news.crunchbase.com/news/what-this-years-seed-funding-tells-us-about-the-startup-future/"
-	var url string = "https://yorkn.info"
-	var txByte []byte = httpRequest(&url)
+	var url string = "https://news.crunchbase.com/news/what-this-years-seed-funding-tells-us-about-the-startup-future/"
+	//var url string = "https://yorkn.info"
+	var od DocTitleBody = getDocument(&url)
+	od = parse(od)
 
-	fmt.Printf("%s", txByte)
+	//var body string = parse(txByte)
+	fmt.Printf("title is %s\n", od.title)
+	fmt.Printf("body is %s\n", od.body)
 
 }
